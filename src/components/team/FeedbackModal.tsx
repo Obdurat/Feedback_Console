12989141ce9a086085fd21e32b7c5e6065a352d8
Anderson Rodrigues da Silva <obrumbata@gmail.com>
2026-05-16 22:56:11 -0300

    feat: Added Feedback Modal, Feedback modal Button, initial modeal form with mock handle submit function and toast

---
+++ src/components/team/FeedbackModal.tsx
@@ -0,0 +1,119 @@
+import type { TeamMember } from "../../types/team.types";
+import { useState } from "react";
+
+interface Props {
+  member: TeamMember | null;
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (data: FeedbackFormData) => void;
+}
+
+export interface FeedbackFormData {
+  type: "POSITIVE" | "IMPROVEMENT";
+  category: string;
+  comment: string;
+}
+
+export const FeedbackModal = ({ member, isOpen, onClose, onSubmit }: Props) => {
+  const [type, setType] = useState<"POSITIVE" | "IMPROVEMENT">("POSITIVE");
+  const [category, setCategory] = useState("");
+  const [comment, setComment] = useState("");
+
+  if (!isOpen || !member) return null;
+
+  const handleSubmit = () => {
+    onSubmit({ type, category, comment });
+    onClose();
+    setCategory("");
+    setComment("");
+    setType("POSITIVE");
+  };
+
+  return (
+    <div className="modal modal-open">
+      <div className="modal-box w-full max-w-lg">
+        <div className="flex flex-col gap-6">
+          <h3 className="font-bold text-lg">Give Feedback to: {member.name}</h3>
+
+          {/* Feedback Type */}
+          <div className="form-control">
+            <label className="label pb-5">
+              <span className="label-text">Feedback Type</span>
+            </label>
+
+            <div className="flex gap-4">
+              <label className="label cursor-pointer gap-2">
+                <input
+                  type="radio"
+                  className="radio radio-success"
+                  checked={type === "POSITIVE"}
+                  onChange={() => setType("POSITIVE")}
+                />
+                <span className="label-text">Positive</span>
+              </label>
+
+              <label className="label cursor-pointer gap-2">
+                <input
+                  type="radio"
+                  className="radio radio-error"
+                  checked={type === "IMPROVEMENT"}
+                  onChange={() => setType("IMPROVEMENT")}
+                />
+                <span className="label-text">Improvement</span>
+              </label>
+            </div>
+          </div>
+
+          {/* Category */}
+          <div className="form-control">
+            <label className="label pr-5">
+              <span className="label-text">Category </span>
+            </label>
+            <select
+              className="select select-bordered"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="">Select category</option>
+              <option>Teamwork</option>
+              <option>Performance</option>
+              <option>Communication</option>
+              <option>Attendance</option>
+            </select>
+          </div>
+
+          {/* Comment */}
+          <div className="form-control">
+            <label className="label pr-2">
+              <span className="label-text">Comments</span>
+            </label>
+            <textarea
+              className="textarea textarea-bordered"
+              rows={4}
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              placeholder="Write your feedback..."
+            />
+          </div>
+        </div>
+        {/* Actions */}
+        <div className="modal-action">
+          <button className="btn" onClick={onClose}>
+            Cancel
+          </button>
+          <button
+            className={`btn ${
+              type === "POSITIVE" ? "btn-success" : "btn-error"
+            }`}
+            onClick={handleSubmit}
+            disabled={!category || !comment}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+
+      <div className="modal-backdrop" onClick={onClose} />
+    </div>
+  );
+};
--- src/components/team/teamTable.tsx
+++ src/components/team/teamTable.tsx
@@ -1,64 +1,97 @@
+import { useState } from "react";
 import type { TeamMember } from "../../types/team.types";
+import { FeedbackModal, type FeedbackFormData } from "./FeedbackModal";
 
 interface Props {
   members: TeamMember[];
 }
 
 export const TeamTable = ({ members }: Props) => {
+  const [selectedMember, setSelectedMember] = useState<TeamMember | null>(null);
+
+  const [toastMessage, setToastMessage] = useState<string | null>(null);
+
+  const handleSubmitFeedback = (data: FeedbackFormData) => {
+    console.log("Feedback submitted ", data);
+    console.log("For member ", selectedMember);
+    setToastMessage("Feedback submitted successfully!");
+
+    setTimeout(() => {
+      setToastMessage(null);
+    }, 3000);
+  };
+
   return (
-    <div className="overflow-x-auto">
-      <table className="table">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Wave</th>
-            <th>Position</th>
-            <th></th>
-          </tr>
-        </thead>
+    <>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Wave</th>
+              <th>Position</th>
+              <th></th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {members.map((member) => (
-            <tr key={member.id}>
-              <td>
-                <input type="checkbox" className="checkbox" />
-              </td>
-
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                      <img
-                        src={"https://ui-avatars.com/api/?name=" + member.name}
-                        alt={member.name}
-                      />
+          <tbody>
+            {members.map((member) => (
+              <tr key={member.id}>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={
+                            "https://ui-avatars.com/api/?name=" + member.name
+                          }
+                          alt={member.name}
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{member.name}</div>
+                      <div className="text-sm opacity-50">{member.status}</div>
                     </div>
                   </div>
-                  <div>
-                    <div className="font-bold">{member.name}</div>
-                    <div className="text-sm opacity-50">{member.status}</div>
-                  </div>
-                </div>
-              </td>
+                </td>
 
-              <td>{member.wave}</td>
+                <td>{member.wave}</td>
 
-              <td>
-                <span className="badge badge-ghost badge-sm">
-                  {member.position}
-                </span>
-              </td>
+                <td>
+                  <span className="badge badge-ghost badge-sm">
+                    {member.position}
+                  </span>
+                </td>
 
-              <td>
-                <button className="btn btn-primary btn-sm">
-                  Give Feedback
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+                <td>
+                  <button
+                    className="btn btn-primary btn-sm"
+                    onClick={() => setSelectedMember(member)}
+                  >
+                    Give Feedback
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <FeedbackModal
+        member={selectedMember}
+        isOpen={!!selectedMember}
+        onClose={() => setSelectedMember(null)}
+        onSubmit={handleSubmitFeedback}
+      />
+
+      {toastMessage && (
+        <div className="toast toast-end">
+          <div className="alert alert-success shadow-lg">
+            <span>{toastMessage}</span>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
