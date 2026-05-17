e1da93cf1582fd75c604fab53cd1180994b8995e
Anderson Rodrigues da Silva <obrumbata@gmail.com>
2026-05-16 22:19:26 -0300

    feat: Added dynamic table for team members, mock api call to get teams, react query into team page

--- src/pages/Teams.tsx
+++ src/pages/Teams.tsx
@@ -1,5 +1,14 @@
 import { TeamTable } from "../components/team/teamTable";
+import { getTeamMembers } from "../api/team";
+import { useQuery } from "@tanstack/react-query";
 
 export const Teams = () => {
-  return <TeamTable />;
+  const { data, isLoading } = useQuery({
+    queryKey: ["team"],
+    queryFn: getTeamMembers,
+  });
+
+  if (isLoading) return <progress className="progress w-56"></progress>;
+
+  return <TeamTable members={data ?? []} />;
 };
